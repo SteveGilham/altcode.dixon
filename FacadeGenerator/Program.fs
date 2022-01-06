b0b5afe0770008be9267b9246a4c4b35417ad718
Steve Gilham <SteveGilham@users.noreply.github.com>
2022-01-06 14:25:09 +0000

    Generate the netstandard2.0 façade, and just commend out obsolete types

---
+++ FacadeGenerator/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+open Mono.Cecil
+
+let netst = AssemblyDefinition.ReadAssembly @"C:\Program Files\dotnet\sdk\6.0.101\ref\netstandard.dll"
+netst.MainModule.GetTypes()
+|> Seq.filter (fun t -> t.IsNested |> not)
+|> Seq.filter (fun t -> t.FullName <> "<Module>")
+|> Seq.iter (fun t -> let n0 = t.FullName
+                      let n = if t.HasGenericParameters
+                              then
+                                let i = n0.IndexOf('`')
+                                let stem = n0.Substring(0, i)
+                                let tag = "<" + String.Join (",",
+                                                             t.GenericParameters
+                                                             |> Seq.map(fun p -> String.Empty)) + ">"
+                                stem + tag
+                              else
+                                n0
+
+                      printfn "[assembly: TypeForwardedTo(typeof(%s))]" n)
