071b170747053cfed099f74c6d5a650efb9b29b3
SteveGilham <SteveGilham@users.noreply.github.com>
2026-08-12 09:56:00 +0100

    Fix compiler noise

--- Build/build.fs
+++ Build/build.fs
@@ -27,3 +27,12 @@ module Build =
     Target.runOrDefault <| Targets.defaultTarget ()
 
     0
+
+[<assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Gendarme.Rules.Performance",
+                                                            "OverrideValueTypeDefaultsRule",
+                                                            Scope = "type", // TypeDefinition
+                                                            Target =
+                                                              "AltCode.Dixon.Actions/T_12Bytes@",
+                                                            Justification =
+                                                              "Compiler generated buffer type")>]
+()
